13e4a183dfac96a43bb1aad602c58adba3e8d4ea
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-11-06 21:42:08 +0500

    Update index.js

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -20,3 +20,10 @@ export async function checkAuthService() {
 
   return data;
 }
+
+
+export async function mediaUploadService(formData) {
+  const { data } = await axiosInstance.post("/media/upload", formData);
+
+  return data;
+}
