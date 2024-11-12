d90ef31955991ffa82946d4e4caf57aee2b34bee
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-11-12 23:21:22 +0500

    Update course-setting.jsx

--- client/src/components/instructor-view/courses/add-new-courses/course-setting.jsx
+++ client/src/components/instructor-view/courses/add-new-courses/course-setting.jsx
@@ -1,9 +1,34 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import React from 'react'
+import { InstructorContext } from '@/context/instructor-context'
+import { mediaUploadService } from '@/services'
+import React, { useContext } from 'react'
 
 const CourseSettings = () => {
+  const { courseLandingFormData, setCourseLandingFormData } =
+    useContext(InstructorContext);
+
+    async function handleImageUploadChange(event) {
+      const selectedImage = event.target.files[0]
+
+      if(selectedImage){
+        const imageFormData = new FormData();
+        imageFormData.append('file', selectedImage)
+
+        try{
+          const response = await mediaUploadService(imageFormData)
+          
+          console.log(response);
+          
+
+        }catch(error){
+          console.log("Select Image error", error);
+          
+        }
+      }
+    }
+
   return (
     <Card>
       <CardHeader>
@@ -12,7 +37,9 @@ const CourseSettings = () => {
       <CardContent>
         <div className='flex flex-col gap-3'>
           <Label>Upload Course Image</Label>
-          <Input type="file"
+          <Input
+          onChange={handleImageUploadChange}
+           type="file"
           accept="image/*"
           className="cursor-pointer"/>
         </div>
