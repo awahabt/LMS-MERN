aad7f180e507d5e116b0190f7b80d0f93bc5712a
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-11-13 22:41:41 +0500

    Update course-setting.jsx

--- client/src/components/instructor-view/courses/add-new-courses/course-setting.jsx
+++ client/src/components/instructor-view/courses/add-new-courses/course-setting.jsx
@@ -1,33 +1,38 @@
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { InstructorContext } from '@/context/instructor-context'
-import { mediaUploadService } from '@/services'
-import React, { useContext } from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { InstructorContext } from "@/context/instructor-context";
+import { mediaUploadService } from "@/services";
+import React, { useContext } from "react";
 
 const CourseSettings = () => {
   const { courseLandingFormData, setCourseLandingFormData } =
     useContext(InstructorContext);
 
-    async function handleImageUploadChange(event) {
-      const selectedImage = event.target.files[0]
+  async function handleImageUploadChange(event) {
+    const selectedImage = event.target.files[0];
 
-      if(selectedImage){
-        const imageFormData = new FormData();
-        imageFormData.append('file', selectedImage)
+    if (selectedImage) {
+      const imageFormData = new FormData();
+      imageFormData.append("file", selectedImage);
 
-        try{
-          const response = await mediaUploadService(imageFormData)
-          
-          console.log(response);
-          
+      try {
+        const response = await mediaUploadService(imageFormData);
 
-        }catch(error){
-          console.log("Select Image error", error);
-          
+        console.log(response);
+        if (response.success) {
+          setCourseLandingFormData({
+            ...courseLandingFormData,
+            image: response.data.url,
+          });
         }
+      } catch (error) {
+        console.log("Select Image error", error);
       }
     }
+  }
+  console.log(courseLandingFormData);
+  
 
   return (
     <Card>
@@ -35,17 +40,22 @@ const CourseSettings = () => {
         <CardTitle>Course Settings</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className='flex flex-col gap-3'>
-          <Label>Upload Course Image</Label>
-          <Input
-          onChange={handleImageUploadChange}
-           type="file"
-          accept="image/*"
-          className="cursor-pointer"/>
-        </div>
+        {courseLandingFormData?.image ? (
+          <img src={courseLandingFormData.image} />
+        ) : (
+          <div className="flex flex-col gap-3">
+            <Label>Upload Course Image</Label>
+            <Input
+              onChange={handleImageUploadChange}
+              type="file"
+              accept="image/*"
+              className="cursor-pointer"
+            />
+          </div>
+        )}
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default CourseSettings
+export default CourseSettings;
