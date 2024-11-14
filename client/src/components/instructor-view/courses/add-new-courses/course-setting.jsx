89810f4b6382c36486d54c46a02968ab9a7481a7
Abdul Wahab Tahir <133004396+awahabt@users.noreply.github.com>
2024-11-14 23:35:10 +0500

    Update course-setting.jsx

--- client/src/components/instructor-view/courses/add-new-courses/course-setting.jsx
+++ client/src/components/instructor-view/courses/add-new-courses/course-setting.jsx
@@ -1,3 +1,4 @@
+import MediaProgressBar from "@/components/media-progress-bar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -6,8 +7,14 @@ import { mediaUploadService } from "@/services";
 import React, { useContext } from "react";
 
 const CourseSettings = () => {
-  const { courseLandingFormData, setCourseLandingFormData } =
-    useContext(InstructorContext);
+  const {
+    courseLandingFormData,
+    setCourseLandingFormData,
+    mediaUploadProgress,
+    setMediaUploadProgress,
+    mediauploadProgressPercentage,
+    setMediauploadProgressPercentage,
+  } = useContext(InstructorContext);
 
   async function handleImageUploadChange(event) {
     const selectedImage = event.target.files[0];
@@ -17,7 +24,11 @@ const CourseSettings = () => {
       imageFormData.append("file", selectedImage);
 
       try {
-        const response = await mediaUploadService(imageFormData);
+        setMediaUploadProgress(true);
+        const response = await mediaUploadService(
+          imageFormData,
+          setMediauploadProgressPercentage
+        );
 
         console.log(response);
         if (response.success) {
@@ -25,6 +36,7 @@ const CourseSettings = () => {
             ...courseLandingFormData,
             image: response.data.url,
           });
+          setMediaUploadProgress(false);
         }
       } catch (error) {
         console.log("Select Image error", error);
@@ -32,13 +44,21 @@ const CourseSettings = () => {
     }
   }
   console.log(courseLandingFormData);
-  
 
   return (
     <Card>
       <CardHeader>
         <CardTitle>Course Settings</CardTitle>
       </CardHeader>
+      <div className="px-5">
+        {mediaUploadProgress ? (
+          <MediaProgressBar
+            isMediaUploading={mediaUploadProgress}
+            progess={mediauploadProgressPercentage}
+          />
+        ) : null}
+      </div>
+
       <CardContent>
         {courseLandingFormData?.image ? (
           <img src={courseLandingFormData.image} />
